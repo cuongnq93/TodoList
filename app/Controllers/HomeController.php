1b9aa076141bbdf54613db64d50c34ef3ed1aa74
cuongnq.93 <cuongnq.93@gmail.com>
2020-06-28 03:03:49 +0700

    Remove some debug code

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -12,18 +12,6 @@ class HomeController extends Controller
      */
     public function indexAction()
     {
-        $viewData = [
-            'type' => []
-        ];
-
-//        $workModel = new Work();
-//        $test = $workModel->insert([
-//           'work_name' => 'Cuong',
-//           'status' => 1,
-//           'start_at' => date('Y-m-d h:i:s', time()),
-//           'end_at' => date('Y-m-d h:i:s', time() + 3600)
-//        ]);
-
-        return $this->view->render('home.index', $viewData);
+        return $this->view->render('home.index');
     }
 }
